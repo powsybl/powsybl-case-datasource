5bd80b6d09d59f3bbc5ffa84f4f894a1c820d6fb
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2022-09-12 15:19:56 +0200

    feat: allow connection to elasticsearch with authentication
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- case-server/src/main/java/com/powsybl/caseserver/elasticsearch/ESConfig.java
+++ case-server/src/main/java/com/powsybl/caseserver/elasticsearch/ESConfig.java
@@ -20,6 +20,7 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.ReadingConverter;
 import org.springframework.data.convert.WritingConverter;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration.TerminalClientConfigurationBuilder;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
@@ -29,6 +30,7 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 
 import java.net.InetSocketAddress;
 import java.util.Arrays;
+import java.util.Optional;
 
 /**
  * A class to configure DB elasticsearch client for metadatas transfer
@@ -65,14 +67,23 @@ public class ESConfig extends AbstractElasticsearchConfiguration {
         @Value("${spring.data.elasticsearch.client.timeout:60}")
         private int timeout;
 
+        @Value("${spring.data.elasticsearch.username:#{null}}")
+        private Optional<String> username;
+
+        @Value("${spring.data.elasticsearch.password:#{null}}")
+        private Optional<String> password;
+
         @Bean
         public RestClients.ElasticsearchRestClient elasticsearchRestClient() {
-            ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+            TerminalClientConfigurationBuilder clientConfiguration = ClientConfiguration.builder()
                     .connectedTo(InetSocketAddress.createUnresolved(esHost, esPort))
-                    .withConnectTimeout(timeout * 1000L).withSocketTimeout(timeout * 1000L)
-                    .build();
+                    .withConnectTimeout(timeout * 1000L).withSocketTimeout(timeout * 1000L);
 
-            return RestClients.create(clientConfiguration);
+            if (username.isPresent() && password.isPresent()) {
+                clientConfiguration = clientConfiguration.withBasicAuth(username.get(), password.get());
+            }
+
+            return RestClients.create(clientConfiguration.build());
         }
     }
 
