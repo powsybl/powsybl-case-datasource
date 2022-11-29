a8945bc5843c1f623c81f8b3a729a7170156a594
Abdelsalem <abdelsalem.hedhili@rte-france.com>
2022-11-29 16:50:47 +0100

    add scheduled cleaning of cases
    Signed-off-by: Abdelsalem <abdelsalem.hedhili@rte-france.com>

---
+++ case-server/src/main/java/com/powsybl/caseserver/ScheduledCaseCleaner.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.caseserver;
+
+import com.powsybl.caseserver.repository.CaseMetadataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+/**
+ * @author Abdelsalem Hedhili <abdelsalem.hedhili at rte-france.com>
+ */
+@Component
+@ComponentScan(basePackageClasses = {CaseService.class, CaseMetadataRepository.class})
+public class ScheduledCaseCleaner {
+
+    @Autowired
+    private CaseMetadataRepository caseMetadataRepository;
+
+    @Autowired
+    private CaseService caseService;
+
+    //every day at 2 AM UTC
+    @Scheduled(cron = "0 * 2 * * ?", zone = "UTC")
+    public void deleteExpiredCases() {
+        LocalDateTime localDateTime = LocalDateTime.now(ZoneOffset.UTC);
+        caseMetadataRepository.findAll().forEach(caseMetadataEntity -> {
+            if (localDateTime.isAfter(caseMetadataEntity.getExpirationDate())) {
+                caseService.deleteCase(caseMetadataEntity.getId());
+                caseMetadataRepository.deleteById(caseMetadataEntity.getId());
+            }
+        });
+
+    }
+}
---
+++ case-server/src/test/java/com/powsybl/caseserver/ScheduledCaseCleanerTest.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.caseserver;
+
+import com.powsybl.caseserver.repository.CaseMetadataEntity;
+import com.powsybl.caseserver.repository.CaseMetadataRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author Abdelsalem Hedhili <abdelsalem.hedhili at rte-france.com>
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScheduledCaseCleanerTest {
+
+    @Autowired
+    public CaseMetadataRepository caseMetadataRepository;
+
+    @Autowired
+    public ScheduledCaseCleaner scheduledCaseCleaner;
+
+    @MockBean
+    public CaseService caseService;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void test() {
+        LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
+        LocalDateTime yesterday = now.minusDays(1);
+        CaseMetadataEntity shouldNotExpireEntity = new CaseMetadataEntity(UUID.randomUUID(), now, now.plusHours(1));
+        CaseMetadataEntity shouldExpireEntity = new CaseMetadataEntity(UUID.randomUUID(), yesterday, yesterday.plusHours(1));
+        caseMetadataRepository.save(shouldExpireEntity);
+        caseMetadataRepository.save(shouldNotExpireEntity);
+        assertEquals(2, caseMetadataRepository.findAll().size());
+        scheduledCaseCleaner.deleteExpiredCases();
+        assertEquals(1, caseMetadataRepository.findAll().size());
+        assertTrue(caseMetadataRepository.findById(shouldNotExpireEntity.getId()).isPresent());
+        assertTrue(caseMetadataRepository.findById(shouldExpireEntity.getId()).isEmpty());
+        verify(caseService, times(1)).deleteCase(shouldExpireEntity.getId());
+    }
+
+}
