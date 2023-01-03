c501ff768559de9845f3ca1484c85874a0394213
Florent MILLOT <75525996+flomillot@users.noreply.github.com>
2023-01-03 10:27:30 +0100

    Rename createCase into duplicateCate (#103)
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- case-server/src/main/java/com/powsybl/caseserver/CaseController.java
+++ case-server/src/main/java/com/powsybl/caseserver/CaseController.java
@@ -143,11 +143,11 @@ public class CaseController {
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "The case has been duplicated"),
             @ApiResponse(responseCode = "404", description = "Source case not found"),
             @ApiResponse(responseCode = "500", description = "An error occurred during the case file duplication")})
-    public ResponseEntity<UUID> createCase(
+    public ResponseEntity<UUID> duplicateCase(
             @RequestParam("duplicateFrom") UUID sourceCaseUuid,
             @RequestParam(value = "withExpiration", required = false, defaultValue = "false") boolean withExpiration) {
-        LOGGER.info("createCase request received with parameter sourceCaseUuid = {}", sourceCaseUuid);
-        UUID newCaseUuid = caseService.createCase(sourceCaseUuid, withExpiration);
+        LOGGER.debug("duplicateCase request received with parameter sourceCaseUuid = {}", sourceCaseUuid);
+        UUID newCaseUuid = caseService.duplicateCase(sourceCaseUuid, withExpiration);
         return ResponseEntity.ok().body(newCaseUuid);
     }
 
@@ -157,7 +157,7 @@ public class CaseController {
             @ApiResponse(responseCode = "200", description = "The case expiration has been removed"),
             @ApiResponse(responseCode = "404", description = "Source case not found")})
     public ResponseEntity<Void> disableCaseExpiration(@PathVariable("caseUuid") UUID caseUuid) {
-        LOGGER.info("disableCaseExpiration request received for caseUuid = {}", caseUuid);
+        LOGGER.debug("disableCaseExpiration request received for caseUuid = {}", caseUuid);
         caseService.disableCaseExpiration(caseUuid);
         return ResponseEntity.ok().build();
     }
